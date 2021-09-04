5d7e6c61a5c78a124b7b86da12b6482ab25a899f
Nesl amd <neslxzhen@gmail.com>
2021-09-04 11:23:46 +0800

    collapsed -> preview

--- app/src/main/java/com/example/labmotion/MainActivity.kt
+++ app/src/main/java/com/example/labmotion/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.labmotion
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.commit
+import androidx.fragment.app.commitNow
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var blankFrag: BlankFragment?= null
+        if (blankFrag == null) blankFrag =  BlankFragment.newInstance() // 如果 home fragment 還沒創建過，創建 fragment，
+
+        supportFragmentManager.commit {
+            if (blankFrag.isAdded) { // 如果 home fragment 已經被 add 過，
+                show(blankFrag); // 顯示它。
+            } else { // 反之，
+                add(R.id.container_main, blankFrag, "BLANK"); // 使用 add 方法。
+            }
+        }
     }
 }
